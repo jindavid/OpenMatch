53fa70b85d1b51af6e25b68ad8a1828d3a61a0fe
jindavid <daaaaavjd@gmail.com>
2021-11-09 02:03:01 +0000

    211108

--- gltr_global_no_att_inference.sh
+++ gltr_global_no_att_inference.sh
@@ -1,12 +1,12 @@
-CUDA_VISIBLE_DEVICES=0 \
+CUDA_VISIBLE_DEVICES=5 \
 python inference.py \
         -task global_no_att \
         -model bert \
         -max_input 1280000 \
-        -test /data2/jindawei/data/dev_valid_1200_subset_from-splitidx-0.top100.jsonl \
+        -test /data2/jindawei/data/test_5193-1200_ANCE.jsonl \
         -vocab bert-base-uncased \
         -pretrain bert-base-uncased \
-        -checkpoint ./checkpoints/gltr_global_n10_no_att_step-10000 \
+        -checkpoint ./checkpoints/gltr_global_n10_no_att_w1_step-10000 \
         -res ./results/gltr_global_no_att_n100_test.trec \
         -max_query_len 32 \
         -max_doc_len 256 \
--- gltr_rank_inference.sh
+++ gltr_rank_inference.sh
@@ -3,11 +3,11 @@ python inference.py \
         -task ranking \
         -model bert \
         -max_input 1280000 \
-        -test /data2/jindawei/data/dev_valid_1200_subset_from-splitidx-0.top100.jsonl \
+        -test /data2/jindawei/data/test_5193-1200_ANCE.jsonl \
         -vocab bert-base-uncased \
         -pretrain bert-base-uncased \
         -checkpoint ./checkpoints/gltr_rank_n10_w1_step-1000 \
         -res ./results/gltr_rank_n100_test.trec \
         -max_query_len 32 \
         -max_doc_len 256 \
-        -batch_size 100
+        -batch_size 32
